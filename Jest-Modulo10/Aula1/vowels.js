80dff278b54f1c704b2f27ffe485cd16f2d63dc6
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-05-19 18:26:10 -0300

    Exercícios Parte 2

---
+++ Jest-Modulo10/Aula1/vowels.js
@@ -0,0 +1,22 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {
+  const arr = str.toUpperCase().split('');
+  const vowels = arr.filter(element => 
+    element === 'A' ||
+    element === 'E' ||
+    element === 'I' ||
+    element === 'O' ||
+    element === 'U'
+  )
+  return vowels.length
+}
+
+module.exports = vowels;
